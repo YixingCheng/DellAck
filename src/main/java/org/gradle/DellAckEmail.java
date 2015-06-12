05127433e8729e7687a9f31c9f5bae9ce4aab235
waldenlaker <waldenlaker@gmail.com>
2015-06-12 04:54:42 +0000

    6/12/2015 2nd commit

--- src/main/java/org/gradle/DellAckEmail.java
+++ src/main/java/org/gradle/DellAckEmail.java
@@ -8,6 +8,9 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+
+import javax.mail.BodyPart;
+import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.MessagingException;
 import javax.mail.Store;
@@ -16,6 +19,7 @@ import javax.mail.Message;
 import javax.mail.NoSuchProviderException;
 import javax.mail.Address;
 import javax.mail.internet.MimeMessage;
+
 import java.text.SimpleDateFormat;
 
 public class DellAckEmail {
@@ -32,6 +36,87 @@ public class DellAckEmail {
         
         Properties props = System.getProperties();
         props.setProperty( "mail.store.protocol", "imaps" );
+        
+        BufferedWriter myWriter = null;
+        try {
+             myWriter = new BufferedWriter( new FileWriter( "/home/ethan/Documents/DellAck.csv" ) );
+             myWriter.write( "\"Sent Date\",\"Subject\",\"From\",\"E-Mail\",\"Phone Number\",\"Skills & Interests\",\"Comments\"\n" );
+        }
+        catch ( IOException myIOE ) {
+             myIOE.printStackTrace();
+        }
+        
+        try{
+        	Session mySession = Session.getDefaultInstance( props, null );
+        	Store myStore = mySession.getStore("imaps");
+        	myStore.connect( HOST, USERNAME, PASSWORD );
+        	
+        	System.out.println( myStore );
+        	
+        	//List all the folders
+        	/*
+        	Folder[] f = myStore.getDefaultFolder().list();
+        	for(Folder fd:f)
+        	    System.out.println(">> " + fd.getName());
+        	*/
+        	
+        	Folder inbox = myStore.getFolder("Test");
+        	inbox.open(Folder.READ_ONLY);
+        	Message messages[] = inbox.getMessages();
+        	System.out.println( messages.length );
+        	
+        	for (Message message:messages){
+        		Address addys[] = message.getFrom();
+        		for (Address addy: addys){
+        			System.out.println(addy.toString());
+        			if( addy.toString().trim().equals("\"r3000ebiz.com\" <admin@r3000ebiz.com>")){
+        				if ( message.getSubject().startsWith( "Pay Mail" ) ){
+        					String messageSubject =  message.getSubject();
+        					System.out.println(messageSubject);
+        					String sentDate = myFormatter.format( message.getSentDate() );
+        					
+        					MimeMessage myMimeMessage = (MimeMessage) message;
+        					//System.out.print(myMimeMessage);
+        					
+        					try{
+        						//Object obj = message.getContent();
+        						Multipart mp = (Multipart) message.getContent();
+        						for(int i=0;i<mp.getCount();i++) {
+        							System.out.println("test1");
+        						    BodyPart bodyPart = mp.getBodyPart(i);
+        						    if ("text/html".equals(bodyPart.getContentType())) {
+        						    	System.out.println("test2");
+        						        String s = (String) bodyPart.getContent();
+        						        System.out.println(s);
+        						    }
+        						}
+        						
+        						
+        						//String messageContent = (String) myMimeMessage.getContent();
+        						//String messageContentLines[] = messageContent.split("\n");
+        						
+        						//for (String line: messageContentLines){
+        						//	System.out.println(line);
+        						//}
+        					}
+        					
+        					catch ( IOException myIOE ) {
+        						myIOE.printStackTrace();
+        					}
+        				}
+        			}
+        		}
+        		
+        		
+        	}
+        	
+        }
+        catch ( NoSuchProviderException e ) {
+        	e.printStackTrace();
+        }
+        catch ( MessagingException e ) {
+            e.printStackTrace();
+        }
         
 	}
 
